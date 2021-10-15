5c84daf653e32151de16550dbac4b2fd6abf448b
Akars <williamli973@gmail.com>
2021-10-15 13:44:30 +0200

    adding express

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
-console.log('this is server')
+const express = require('express')
+const session = require('express-session')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+})
