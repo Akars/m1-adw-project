85cd344a815c10033b91aafced4dbc2cc36faed4
Akars <williamli973@gmail.com>
2021-11-03 22:01:17 +0100

    card vue

---
+++ my-project/src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+    <button class="normal"
+            :class="type"
+            @click="callback($event)"
+            >
+    <slot></slot>
+    </button>
+</template>
+<script>
+
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
